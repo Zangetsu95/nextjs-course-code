63aef348b5df742131fdb14c6170520ebe966c61
Zangetsu95 <aminehoussane@gmail.com>
2023-07-05 15:15:45 +0200

    Adding Regular React Components

--- project-1/pages/index.js
+++ project-1/pages/index.js
@@ -1,4 +1,9 @@
+import { getFeaturedEvents } from '../dummy-data'
+
 function HomePage() {
+
+    const featuredEvents = getFeaturedEvents()
+
     return (
         <>
             <h1>Home Page</h1>
