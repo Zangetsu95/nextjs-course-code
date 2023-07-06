c5ade47950a4a4a254e63edd61ae4f320f08512a
Zangetsu95 <aminehoussane@gmail.com>
2023-07-06 13:16:02 +0200

    the Filtered Events Page Progammatically

--- project-1/components/events/events-search.js
+++ project-1/components/events/events-search.js
@@ -1,21 +1,34 @@
+import { useRef } from "react"
 import Button from "../ui/button"
 import classes from './event-search.module.css'
 
 function EventsSearch(props) {
 
+    const yearInputRef = useRef()
+    const monthInputRef = useRef()
+
+    function submitHandler(event) {
+        event.preventDefault()
+
+        const selectedYear = yearInputRef.current.value
+        const selectedMotnh = monthInputRef.current.value
+
+        props.onSearch(selectedYear, selectedMotnh)
+    }
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.controls}>
                 <div className={classes.control}>
-                    <label htmlFor="year">Year</label>
-                    <select id="year">
+                    <label htmlFor="year" >Year</label>
+                    <select id="year" ref={yearInputRef}>
                         <option value='2021'>2021</option>
                         <option value='2022'>2022</option>
                     </select>
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor="motnh">Month</label>
-                    <select id="month">
+                    <label htmlFor="motnh" >Month</label>
+                    <select id="month" ref={monthInputRef}>
                         <option value='1'>January</option>
                         <option value='2'>February</option>
                         <option value='3'>March</option>
