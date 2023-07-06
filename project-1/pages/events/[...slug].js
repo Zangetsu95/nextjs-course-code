d4017a7cb477927333367e1ebf9f8d0950cda428
Zangetsu95 <aminehoussane@gmail.com>
2023-07-06 13:28:28 +0200

    Extracting Data on the Catch-All Page

--- project-1/pages/events/[...slug].js
+++ project-1/pages/events/[...slug].js
@@ -1,4 +1,37 @@
+import { useRouter } from "next/router"
+import { getFilteredEvents } from '../../dummy-data'
+
 function FilteredEventsPage() {
+
+    const router = useRouter()
+
+    const filterData = router.query.slug
+
+    console.log('filterData :>> ', filterData);
+
+    if (!filterData) {
+        return <p className="center" >Loading...</p>
+    }
+
+    const filteredYear = filterData[0]
+    const filteredMonth = filterData[1]
+
+    const numYear = +filteredYear
+    const numMotnh = +filteredMonth
+
+    if (isNaN(numYear) || isNaN(numMotnh) || numYear > 2030 || numYear < 2021 || numMotnh < 1 || numMotnh > 12) {
+        return <p>Invalid filter.Please adjust your values !</p>
+    }
+
+    const filteredEvents = getFilteredEvents({
+        year: numYear,
+        month: numMotnh
+    })
+
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return <p>No events found for the chosen filter !</p>
+    }
+
     return (
         <>
             <h1>Slug of an event</h1>
